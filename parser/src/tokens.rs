307044ee0df53fd5bcd91cbed1bbdbda1d47fdf3
Ariel Young <ariyoung@pm.me>
2023-07-06 18:48:04 -0700

    Moved tokens.rs to parser

---
+++ parser/src/tokens.rs
@@ -0,0 +1,119 @@
+use logos::{Logos, Lexer};
+use ordered_float::OrderedFloat;
+
+#[derive(Logos, Clone, Debug, PartialEq, Eq)]
+pub enum Token {
+    #[token("fn")]
+    Fn,
+    #[token("if")]
+    If,
+    #[token("else")]
+    Else,
+    #[token("for")]
+    For,
+    #[token("while")]
+    While,
+    #[token("break")]
+    Break,
+    #[token("continue")]
+    Continue,
+    #[token("in")]
+    In,
+    #[token("let")]
+    Let,
+    #[token("var")]
+    Var,
+
+    #[token("bool")]
+    Bool,
+    #[token("int")]
+    Int,
+    #[token("float")]
+    Float,
+    #[token("string")]
+    String,
+    #[token("cmd")]
+    Command,
+
+    #[regex("[_a-zA-Z][_0-9a-zA-Z]*", |lex| lex.slice().parse().ok())]
+    Identifier(String),
+
+    #[token("+")]
+    Plus,
+    #[token("-")]
+    Minus,
+    #[token("*")]
+    Asterisk,
+    #[token("/")]
+    Slash,
+    #[token("%")]
+    Modulus,
+    #[token("<")]
+    Less,
+    #[token("<=")]
+    LessEqual,
+    #[token(">")]
+    Greater,
+    #[token(">=")]
+    GreaterEqual,
+    #[token("==")]
+    EqualEqual,
+    #[token("!=")]
+    NotEqual,
+    #[token("&&")]
+    LogicalAnd,
+    #[token("||")]
+    LogicalOr,
+
+    #[token("(")]
+    LParen,
+    #[token(")")]
+    RParen,
+    #[token("{")]
+    LBrace,
+    #[token("}")]
+    RBrace,
+    #[token("[")]
+    LBracket,
+    #[token("]")]
+    RBracket,
+    #[token(":")]
+    Colon,
+    #[token(";")]
+    SemiColon,
+    #[token("=")]
+    Equal,
+    #[token(",")]
+    Comma,
+    #[token("->")]
+    Arrow,
+    #[token(".")]
+    Dot,
+    #[token("#")]
+    Hash,
+    #[token("&")]
+    Ampersand,
+    #[token("!")]
+    ExclamationMark,
+    #[token("?")]
+    QuestionMark,
+    #[token("|")]
+    Pipe,
+
+    #[regex("true|false", |tok| tok.slice().parse())]
+    BoolLit(bool),
+    #[regex(r#""(\\.|[^\\"\n])*""#, |tok| tok.slice()[1..tok.slice().len()-1].parse())]
+    StrLit(String),
+    #[regex(r"\d+", |tok| tok.slice().parse())]
+    IntLit(i64),
+    #[regex(r"\d+\.\d*", |tok| tok.slice().parse())]
+    FloatLit(OrderedFloat<f64>),
+    #[regex(r"`(\\.|[^\\`\n])*`", |tok| tok.slice()[1..tok.slice().len()-1].parse())]
+    CommandLit(String),
+    
+    #[regex(r"//.*\n?", logos::skip)]
+    #[regex(r"[ \t\n\f]+", logos::skip)]
+    #[error]
+    Error,
+}
+
