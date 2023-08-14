d0c74b83573d8c725c70e9e46d8c3fd9f391cb32
Ariel Young <ariyoung@pm.me>
2023-08-13 17:56:45 -0700

    mostly finish interpreter

--- interpreter/src/lib.rs
+++ interpreter/src/lib.rs
@@ -1,27 +1,113 @@
 #![feature(if_let_guard)]
+#![feature(try_trait_v2)]
 
 mod data;
 mod type_checker;
 
-use clam_common::ast::{Identifier, Expr, Block, Span, UnOp, BinOp, FnCall, FnDef, Statement};
-use data::{ClamData, ClamRuntimeError, ClamString, GcRef, Heap, ErrorSource::*, ClamUserType};
+use std::{
+    cell::RefCell,
+    convert::Infallible,
+    ops::{ControlFlow, FromResidual, Try},
+    sync::{atomic::AtomicBool, Arc},
+};
+
+use clam_common::ast::{
+    Assign, BinOp, Block, Conditional, Expr, FnCall, FnDef, Identifier, Let, Span, Statement, UnOp,
+    WhileLoop,
+};
+use data::{
+    ClamBool, ClamData, ClamRuntimeError, ClamString, ClamUserType, ErrorSource::*, GcRef, Heap,
+};
 use derive_more::Constructor;
 use im_rc::HashMap;
 
+use crate::data::ErrorSource;
 
+// type Result<T> = std::result::Result<T, ClamRuntimeError>;
+pub enum EvalResult<T, E> {
+    Ok(T),
+    Err(E),
+    Return(ClamData),
+    Break,
+    Continue,
+}
 
-type Result<T> = std::result::Result<T, ClamRuntimeError>;
+use EvalResult::*;
+
+#[allow(dead_code)]
+impl<T, E> EvalResult<T, E>
+where
+    E: std::fmt::Debug,
+{
+    pub fn map_err<NewE>(self, f: impl FnOnce(E) -> NewE) -> EvalResult<T, NewE> {
+        match self {
+            Err(e) => Err(f(e)),
+            Ok(v) => Ok(v),
+            Return(e) => Return(e),
+            Break => Break,
+            Continue => Continue,
+        }
+    }
+    pub fn unwrap(self) -> T {
+        match self {
+            Ok(val) => {
+                return val;
+            }
+            Err(e) => {
+                core::result::Result::<(), E>::Err(e).unwrap();
+                unreachable!("unreachable");
+            }
+            Return(val) => panic!("Called `unwrap` on EvalResult::Return({val:?})"),
+            Break => panic!("Called `unwrap` on EvalResult::Break"),
+            Continue => panic!("Called `unwrap` on EvalResult::Continue"),
+        }
+    }
+}
+
+impl<T, E> Try for EvalResult<T, E> {
+    type Output = T;
+    type Residual = EvalResult<Infallible, E>;
+
+    fn from_output(output: Self::Output) -> Self {
+        Self::Ok(output)
+    }
+
+    fn branch(self) -> std::ops::ControlFlow<Self::Residual, Self::Output> {
+        match self {
+            Ok(v) => ControlFlow::Continue(v),
+            Err(e) => ControlFlow::Break(Err(e)),
+            Return(v) => ControlFlow::Break(Return(v)),
+            Break => ControlFlow::Break(Break),
+            Continue => ControlFlow::Break(Continue),
+        }
+    }
+}
+
+impl<T, E> FromResidual<EvalResult<Infallible, E>> for EvalResult<T, E> {
+    fn from_residual(residual: EvalResult<Infallible, E>) -> Self {
+        match residual {
+            Ok(_) => unreachable!("from residual on infallible"),
+            Err(e) => Err(e),
+            Return(v) => Return(v),
+            Break => Break,
+            Continue => Continue,
+        }
+    }
+}
+
+type Result<T> = EvalResult<T, ClamRuntimeError>;
 
 trait ErrSpan {
     fn add_loc(self, start: usize, end: usize) -> Self;
     fn likely_has_loc(&self) -> bool;
 
     fn map_no_loc(self, start: usize, end: usize) -> Self
-    where Self: Sized {
+    where
+        Self: Sized,
+    {
         if self.likely_has_loc() {
             self
-        }
-        else {
+        } else {
             self.add_loc(start, end)
         }
     }
@@ -29,7 +115,7 @@ trait ErrSpan {
 
 impl<T> ErrSpan for Result<T> {
     fn add_loc(self, start: usize, end: usize) -> Self {
-        self.map_err(|ClamRuntimeError{ src, .. }| ClamRuntimeError{
+        self.map_err(|ClamRuntimeError { src, .. }| ClamRuntimeError {
             loc: (start, end),
             src,
         })
@@ -37,144 +123,222 @@ impl<T> ErrSpan for Result<T> {
 
     fn likely_has_loc(&self) -> bool {
         match self {
-            Ok(_) => true,
             Err(e) => e.loc == (0, 0),
+            _ => true,
         }
     }
 }
 
-#[derive(Constructor)]
-struct Ctx<'a> {
-    heap: &'a mut Heap<ClamUserType>,
+#[derive(Constructor, Clone)]
+struct Ctx {
+    heap: Arc<RefCell<Heap<ClamUserType>>>,
     vars: HashMap<Identifier, ClamData>,
     funcs: HashMap<Identifier, FnDef>,
+    current_loop: Option<Arc<AtomicBool>>,
 }
 
-impl Ctx<'_> {
+#[allow(dead_code)]
+impl Ctx {
     pub fn to_heap(&mut self, data: ClamUserType) -> GcRef<ClamUserType> {
-        self.heap.move_to_heap(data)
+        self.heap.borrow_mut().move_to_heap(data)
+    }
+
+    fn with_vars(&self, vars: HashMap<Identifier, ClamData>) -> Self {
+        Ctx {
+            heap: self.heap.clone(),
+            vars,
+            funcs: self.funcs.clone(),
+            current_loop: self.current_loop.clone(),
+        }
     }
 }
 
 #[allow(dead_code)]
-fn eval_expr<'b>(exp: &Span<Box<Expr>>, ctx: &mut Ctx<'b>) -> Result<ClamData> {
+fn eval_expr<'b>(exp: &Span<Box<Expr>>, ctx: &mut Ctx) -> Result<ClamData> {
     let (start, exp, end) = exp;
     let (start, end) = (*start, *end);
 
     match exp.as_ref() {
         Expr::Literal(lit) => Ok(match lit {
-            clam_common::ast::Literal::Int(i)      => ClamData::Int((*i).into()),
-            clam_common::ast::Literal::Float(f)    => ClamData::Float((*f).into()),
-            clam_common::ast::Literal::Bool(b)     => ClamData::Bool((*b).into()),
-            clam_common::ast::Literal::String(s)   => ClamData::String(ClamString((s.clone()).into())),
+            clam_common::ast::Literal::Int(i) => ClamData::Int((*i).into()),
+            clam_common::ast::Literal::Float(f) => ClamData::Float((*f).into()),
+            clam_common::ast::Literal::Bool(b) => ClamData::Bool((*b).into()),
+            clam_common::ast::Literal::String(s) => {
+                ClamData::String(ClamString((s.clone()).into()))
+            }
             clam_common::ast::Literal::Command(_c) => todo!("Evaluate commands"),
         }),
 
-        Expr::BinOp(BinOp{ op, lhs, rhs }) => {
+        Expr::BinOp(BinOp { op, lhs, rhs }) => {
             let lhs = eval_expr(&lhs, ctx)?;
             let rhs = eval_expr(&rhs, ctx)?;
+            let cbool = |b| ClamData::Bool(ClamBool(b));
 
             Ok(match op {
                 clam_common::ast::BinaryOperator::Plus => lhs + rhs,
                 clam_common::ast::BinaryOperator::Minus => lhs - rhs,
                 clam_common::ast::BinaryOperator::Times => lhs * rhs,
                 clam_common::ast::BinaryOperator::Div => (lhs / rhs).add_loc(start, end)?,
-                clam_common::ast::BinaryOperator::Mod => todo!(),
-                clam_common::ast::BinaryOperator::Lt => todo!(),
-                clam_common::ast::BinaryOperator::Lte => todo!(),
-                clam_common::ast::BinaryOperator::Gt => todo!(),
-                clam_common::ast::BinaryOperator::Gte => todo!(),
-                clam_common::ast::BinaryOperator::Eq => todo!(),
-                clam_common::ast::BinaryOperator::Ne => todo!(),
-                clam_common::ast::BinaryOperator::And => todo!(),
-                clam_common::ast::BinaryOperator::Or => todo!(),
-                clam_common::ast::BinaryOperator::BitAnd => todo!(),
-                clam_common::ast::BinaryOperator::BitOr => todo!(),
+                clam_common::ast::BinaryOperator::Mod => (lhs % rhs).add_loc(start, end)?,
+                clam_common::ast::BinaryOperator::Lt  => cbool(lhs < rhs),
+                clam_common::ast::BinaryOperator::Lte => cbool(lhs <= rhs),
+                clam_common::ast::BinaryOperator::Gt  => cbool(lhs > rhs),
+                clam_common::ast::BinaryOperator::Gte => cbool(lhs >= rhs),
+                clam_common::ast::BinaryOperator::Eq  => cbool(lhs == rhs),
+                clam_common::ast::BinaryOperator::Ne  => cbool(lhs != rhs),
+                clam_common::ast::BinaryOperator::And => cbool(ensure_bool(&lhs) && ensure_bool(&rhs)),
+                clam_common::ast::BinaryOperator::Or  => cbool(ensure_bool(&lhs) || ensure_bool(&rhs)),
+                clam_common::ast::BinaryOperator::BitAnd => lhs & rhs,
+                clam_common::ast::BinaryOperator::BitOr  => lhs | rhs,
+                clam_common::ast::BinaryOperator::BitXor => lhs ^ rhs,
             })
-        },
+        }
 
-        Expr::UnOp(UnOp{ op , rhs }) => {
+        Expr::UnOp(UnOp { op, rhs }) => {
             let res = eval_expr(&rhs, ctx)?;
             Ok(match op {
-                clam_common::ast::UnaryOperator::Not => !res,
-                clam_common::ast::UnaryOperator::BitInvert => todo!(),
-                clam_common::ast::UnaryOperator::Negate => todo!(),
+                clam_common::ast::UnaryOperator::Not
+                 | clam_common::ast::UnaryOperator::BitInvert => !res,
+                clam_common::ast::UnaryOperator::Negate => -res,
             })
-        },
+        }
 
         Expr::Identifier(i) => match ctx.vars.get(&i) {
             Some(val) => Ok(val.clone()),
             None => VariableNotFound.with_loc(start, end),
         },
-        
-        Expr::FnCall(FnCall{ id, args }) => {
+
+        Expr::FnCall(FnCall { id, args }) => {
             let (start, id, end) = id;
 
-            let arg_vals: Vec<_> = args.into_iter()
-                .map(|exp| eval_expr(exp, ctx))
-                .collect::<Result<_>>()?;
+            let mut arg_vals = Vec::new();
+            for arg in args {
+                arg_vals.push(eval_expr(arg, ctx)?);
+            }
 
             let Some(def) = ctx.funcs.get(&id) else {
                 return FunctionNotFound.with_loc(*start, *end);
             };
 
-            let mappings = def.param_list.iter()
+            let mappings = def
+                .param_list
+                .iter()
                 .map(|((_, id, _), _)| id.clone())
                 .zip(arg_vals.into_iter());
             let mut new_vars = ctx.vars.clone();
             new_vars.extend(mappings);
 
-            let mut new_ctx = Ctx{ heap: ctx.heap, vars: new_vars, funcs: ctx.funcs.clone() };
-            
-            eval_expr(&def.body, &mut new_ctx)
-        },
+            let mut new_ctx = ctx.with_vars(new_vars);
+
+            match eval_expr(&def.body, &mut new_ctx) {
+                Return(val) => Ok(val),
+                other => other,
+            }
+        }
 
         Expr::LambdaDef(_) => todo!(),
 
-        Expr::Block(block) => {
-            let new_ctx = Ctx{ heap: ctx.heap, vars: ctx.vars.clone(), funcs: ctx.funcs.clone() };
-            eval_block(block, new_ctx)
-        },
+        Expr::Block(block) => eval_block(block, ctx),
 
-        Expr::Conditional(_) => todo!(),
+        Expr::Conditional(Conditional { cond, body, r#else }) => {
+            let res = eval_expr(cond, ctx)?;
+            match res {
+                ClamData::Bool(ClamBool(true)) => eval_block(body, ctx),
+                ClamData::Bool(ClamBool(false)) if let Some(r#else) = r#else 
+                    => eval_block(r#else, ctx),
+                _ => Ok(ClamData::Empty)
+            }
+        }
 
-        Expr::WhileLoop(_) => todo!(),
+        Expr::WhileLoop(WhileLoop { cond, body }) => {
+            // let mut cond = eval_expr(cond, ctx)?;
+            let mut res = ClamData::Empty;
+
+            while ensure_bool(&eval_expr(cond, ctx)?) {
+                match eval_block(body, ctx) {
+                    Ok(data) => res = data,
+                    Break => return Ok(ClamData::Empty),
+                    // somewhere in the eval process, we hit a continue.
+                    // in this case, we want to do nothing and execute the next iteration
+                    Continue => (),
+                    // for Err(_), Return(_), we want to interrupt iteration and short-circuit
+                    other => return other,
+                };
+            }
+
+            Ok(res)
+        }
 
         Expr::ForLoop(_) => todo!(),
     }
 }
 
-fn eval_block(block: &Block, mut ctx: Ctx) -> Result<ClamData> {
+fn ensure_bool(data: &ClamData) -> bool {
+    match data {
+        ClamData::Bool(ClamBool(b)) => *b,
+        _ => unreachable!("found non-boolean type where boolean was expected"),
+    }
+}
+
+fn eval_block(block: &Block, ctx: &mut Ctx) -> Result<ClamData> {
+    // we need to figure out a way to update variables in the enclosing scope, but discard new variables declared within this block
+    // clone the context
+    let mut new_ctx = ctx.clone();
+
     for (start, line, end) in &block.body {
-        use Statement::Return;
-        let _ = match line {
-            Return(expr) => return eval_expr(&expr, &mut ctx),
-            other => eval_statement(&other, &mut ctx),
-        }.map_no_loc(*start, *end)?;
+        let _ = eval_statement(line, &mut new_ctx).map_no_loc(*start, *end)?;
     }
 
     if let Some((start, last, end)) = &block.last {
-        eval_statement(&last, &mut ctx)
-            .map_no_loc(*start, *end)
-    }
-    else {
+        eval_statement(&last, &mut new_ctx).map_no_loc(*start, *end)
+    } else {
         Ok(ClamData::Empty)
     }
 }
 
 fn eval_statement(stmt: &Statement, ctx: &mut Ctx) -> Result<ClamData> {
-    todo!()
-}
+    use ClamData::*;
+    match stmt {
+        Statement::FnDef(_) => unreachable!("Function def should be removed"),
+        Statement::Let(Let {
+            id,
+            r#type: _,
+            expr,
+        }) => {
+            let (_, id, _) = id;
+            let res = expr.as_ref().map(|exp| eval_expr(&exp, ctx)).unwrap()?;
 
+            ctx.vars.insert(id.clone(), res);
+
+            Ok(Empty)
+        }
+        Statement::Assign(Assign { id, expr }) => {
+            let (var_start, id, var_end) = id;
+
+            if !ctx.vars.contains_key(id) {
+                return ErrorSource::VariableNotFound.with_loc(*var_start, *var_end);
+            }
+
+            let res = eval_expr(expr, ctx)?;
+            ctx.vars[id] = res;
+
+            Ok(Empty)
+        }
+        Statement::Expr(e) => eval_expr(e, ctx),
+        Statement::Break => Break,
+        Statement::Return(expr) => Return(match expr {
+            Some(expr) => eval_expr(expr, ctx)?,
+            None => ClamData::Empty,
+        }),
+    }
+}
 
 #[cfg(test)]
 mod test {
-    use std::hash::Hash;
-
+    use super::*;
+    use crate::{data::*, eval_expr};
     use clam_common::ast::*;
     use im_rc::HashMap;
-    use crate::{data::*, eval_expr};
-    use super::*;
 
     fn bin_op(op: BinaryOperator, lhs: Span<Box<Expr>>, rhs: Span<Box<Expr>>) -> Span<Box<Expr>> {
         (0, Box::new(Expr::BinOp(BinOp { op, lhs, rhs })), 0)
@@ -188,16 +352,14 @@ mod test {
         ClamData::Int(i.into())
     }
 
-
     #[test]
     fn eval_arith_expr() {
         use BinaryOperator::*;
         let ast = bin_op(Plus, cint_lit(1), cint_lit(2));
-        let mut heap = Heap::default();
-        let mut ctx = Ctx::new(&mut heap, HashMap::new(), HashMap::new());
+        let heap = Arc::new(RefCell::new(Heap::default()));
+        let mut ctx = Ctx::new(heap, HashMap::new(), HashMap::new(), None);
 
         let res = eval_expr(&ast, &mut ctx).unwrap();
         assert_eq!(res, cint_data(3));
     }
-
 }
