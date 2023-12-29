9ee76cd8642a20838711b604c896e43b52724faa
Ariel Young <ariyoung@pm.me>
2023-12-29 03:04:03 -0800

    add test data, builtins file

---
+++ src/test.rs
@@ -0,0 +1,19 @@
+// use serde::Deserialize;
+use crate::interpret_file_contents;
+extern crate pretty_assertions;
+use self::pretty_assertions::{assert_eq, assert_ne, assert_str_eq};
+
+#[datatest::files("test_data/", {
+  // Pattern is defined via `in` operator. Every file from the `directory` above will be matched
+  // against this regular expression and every matched file will produce a separate test.
+  input in r"^(.*)\.input\.cm",
+  // Template defines a rule for deriving dependent file name based on captures of the pattern.
+  output = r"${1}.output.txt",
+})]
+#[test]
+fn files_test_strings(input: &str, output: &str) {
+    let res = interpret_file_contents(input).unwrap();
+    println!("res is '{}'", res);
+    println!("output is '{}'", output);
+    assert_str_eq!(interpret_file_contents(input).unwrap(), output);
+}
